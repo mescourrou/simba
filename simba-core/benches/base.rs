2ae136d61b84cd0548205e0353808f1f301fc591
Maxime Escourrou <maxime.escourrou@laas.fr>
2026-04-17 17:02:22 +0200

    Add performance benchmark

---
+++ simba-core/benches/base.rs
@@ -0,0 +1,53 @@
+use std::path::Path;
+use criterion::{criterion_group, criterion_main, Criterion};
+use simba::{logger::LogLevel, simulator::{Simulator, SimulatorConfig}};
+extern crate simba;
+
+fn run_benchmark(c: &mut Criterion, name: &str, config: SimulatorConfig) {
+    let mut simulator = Simulator::from_config(
+        &config,
+        None,
+    )
+    .map_err(|e| {
+        println!("Error while loading config: {}", e.detailed_error());
+        e
+    })
+    .unwrap();
+
+    c.bench_function(name, |b| {
+        b.iter(|| {
+            simulator.reset(None).unwrap();
+            simulator.run().expect("Error while running simulation");
+        })
+    });
+}
+
+fn config_monothread(c: &mut Criterion) {
+    let mut config = SimulatorConfig::load_from_path(Path::new("test_config/config.yaml")).unwrap();
+    config.max_time = 60.0;
+    config.log.log_level = LogLevel::Off;
+    config.results = None;
+    config.optimization.threads = 1;
+    run_benchmark(c, "run_config_monothread", config);
+}
+
+fn config_multithread(c: &mut Criterion) {
+    let mut config = SimulatorConfig::load_from_path(Path::new("test_config/config.yaml")).unwrap();
+    config.max_time = 60.0;
+    config.log.log_level = LogLevel::Off;
+    config.results = None;
+    config.optimization.threads = 0;
+    run_benchmark(c, "run_config_multithread", config);
+}
+
+fn config_pooled(c: &mut Criterion) {
+    let mut config = SimulatorConfig::load_from_path(Path::new("test_config/config.yaml")).unwrap();
+    config.max_time = 60.0;
+    config.log.log_level = LogLevel::Off;
+    config.results = None;
+    config.optimization.threads = 2;
+    run_benchmark(c, "run_config_pooled", config);
+}
+
+criterion_group!(benches_config, config_monothread, config_multithread, config_pooled);
+criterion_main!(benches_config);
